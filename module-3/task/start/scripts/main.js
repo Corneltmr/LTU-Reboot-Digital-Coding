34db1b109484def8386b56c9c80ac6d2e568b4c1
Corneltmr <timaru_cornel@yahoo.com>
2021-10-09 20:28:57 +0100

    Update main.js
    Add practice comments, variables, console.log's, document.write and data types

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,56 @@
+// This is a single line comment
+
+/**
+ *  This is a multiple
+ *  line comment
+ * */
+
+/** 
+ *     let age = 31
+ *     const name = 'Cornel'
+ *     var address = '15 highthorne';
+ * 
+    * let $variable = 0;
+    * let variable = 0;
+    * let_variable = 0;
+    * 
+    * let firstName = ''Cornel'';
+    * let lastName = ''Timaru''; 
+*/
+
+const productName = 'Baked Beans';
+let price = 1.27;
+let quantity = 4;
+let inStock = true;
+let discountAmount = 10;
+
+// console.log( 'Product Name:', productName );
+// console.log( 'Price', price );
+// console.log( 'Quantity', quantity );
+// console.log( 'In Stock', inStock );
+// console.log( 'Disocunt', discountAmount );
+
+// console.log( typeof productName );
+// console.log( typeof price );
+// console.log( typeof quantity );
+// console.log( typeof inStock );
+// console.log( typeof discountAmount );
+
+// document.write( ''<p> + productName + ''</p>);
+
+// let myName = '';
+// let numOfKids = 0;
+// let isMarried;
+
+// let newName = 'Cornel';
+// let newNumOfKids = '1';
+
+// console.log( Boolean(myName) );
+// console.log( Boolean(numOfKids) );
+// console.log( Boolean(isMarried) );
+// console.log( Boolean(null) );
+// console.log( Boolean(undefined) );
+// console.log( '--------');
+// console.log( Boolean(newName) );
+// console.log( Boolean(newNumOfKids) );
+
